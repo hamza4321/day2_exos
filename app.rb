5a61a9c4d8303bcbfc8af692414d29c6dbcdaa0a
hamza <hsehnoun@hotmail.com>
2018-01-23 15:32:30 +0100

    exos-day3

--- app.rb
+++ app.rb
@@ -1,2 +1,24 @@
 10.times {puts "hello world"}
 
+for n in 1..10 
+	puts "putain de monde"
+end
+
+n=1
+	while n <10
+		puts "allo le monde"
+		n +=1
+end
+
+f= 1
+	until f >10
+		puts "monde de coquins"
+		f +=1
+end
+
+
+(0..10).each do 
+	puts "drolzsee de monde"
+end
+
+	
